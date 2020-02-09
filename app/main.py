7abbea70cbf4d949765093946d5aca945e2e790f
ApppleTurnover <apppleturnover@gmail.com>
2020-02-09 17:06:07 +1000

    Check out some features in flask

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, make_response, abort
 
 app = Flask(__name__)
 
+some_users = ['Athon', 'Apple', 'Greys', 'Jake']
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', title='Rofl')
+    response = make_response('<h1>Some Text</h1>')
+    response.set_cookie('answer', '41')
+    return response
+
+
+@app.route('/user/<user>')
+def get_user(user):
+    if user not in some_users:
+        abort(404)
+    return f'<h1>Hello, {user}</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
